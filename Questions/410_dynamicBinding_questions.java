3263852e39437c8c82a1660a153a2fd61dfe5f48
scahail <scahail@utexas.edu>
2013-05-01 22:31:39 -0500

    Update 410_dynamicBinding_questions.java
    Adding dynamic binding questions for java

---
+++ Questions/410_dynamicBinding_questions.java
@@ -0,0 +1,10 @@
+/**
+ * DyanmicBinding.java Questions
+ * =============================
+ * 1. In Java, is dynamic binding enabled or disabled by default?
+ * 
+ * 2. In what 4 cases is dynamic binding turned off in Java? 
+ * 
+ * 3. What advantage is there to dynamic binding being turned off? What can the compiler then do? 
+ * 
+ * /
